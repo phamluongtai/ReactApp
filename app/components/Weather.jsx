062dfd7a473bfb45a97dce061a683b112934d589
unknown <phamluongtai@gmail.com>
2017-10-22 20:35:48 -0700

    Initial commit

---
+++ app/components/Weather.jsx
@@ -0,0 +1,64 @@
+var React = require('react');
+var WeatherForm = require('app/components/WeatherForm');
+var WeatherMessage = require('app/components/WeatherMessage');
+var ErrorModal = require('app/components/ErrorModal');
+var openWeatherMap = require('openWeatherMap');
+
+var Weather = React.createClass({
+  getInitialState: function () {
+    return {
+      isLoading: false
+    }
+  },
+  handleSearch: function (location) {
+    var that = this;
+
+    this.setState({
+      isLoading: true,
+      errorMessage: undefined
+    });
+
+    openWeatherMap.getTemp(location).then(function (temp) {
+      that.setState({
+        location: location,
+        temp: temp,
+        isLoading: false
+      });
+    }, function (e) {
+      that.setState({
+        isLoading: false,
+        errorMessage: e.message
+      });
+    });
+  },
+  render: function () {
+    var {isLoading, temp, location, errorMessage} = this.state;
+
+    function renderMessage () {
+      if (isLoading) {
+        return <h3 className="text-center">Fetching weather...</h3>;
+      } else if (temp && location) {
+        return <WeatherMessage temp={temp} location={location}/>;
+      }
+    }
+
+    function renderError () {
+      if (typeof errorMessage === 'string') {
+        return (
+          <ErrorModal message={errorMessage}/>
+        )
+      }
+    }
+
+    return (
+      <div>
+        <h1 className="text-center page-title">Get Weather</h1>
+        <WeatherForm onSearch={this.handleSearch}/>
+        {renderMessage()}
+        {renderError()}
+      </div>
+    )
+  }
+});
+
+module.exports = Weather;
